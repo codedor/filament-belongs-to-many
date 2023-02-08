95ee0f60c2df7435a4badfe2231a761f4c942087
AngryMoustache <sander.vandamme11@gmail.com>
2023-02-08 14:10:08 +0100

    Initial

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace VendorName\Skeleton\Database\Factories;
+namespace Codedor\BelongsToMany\Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
--- src/Providers/BelongsToManyServiceProvider.php
+++ src/Providers/BelongsToManyServiceProvider.php
@@ -1,16 +1,16 @@
 <?php
 
-namespace VendorName\Skeleton\Providers;
+namespace Codedor\BelongsToMany\Providers;
 
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
-class SkeletonServiceProvider extends PackageServiceProvider
+class BelongsToManyServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
         $package
-            ->name('skeleton')
+            ->name('filament-belongs-to-many')
             ->setBasePath(__DIR__ . '/../')
             ->hasConfigFile()
             ->hasMigration('create_package_table');
