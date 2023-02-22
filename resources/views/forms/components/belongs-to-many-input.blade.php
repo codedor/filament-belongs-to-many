b6fbfa0b95dcd11969f19ebc4f9a77b0b36a6d10
AngryMoustache <sander.vandamme11@gmail.com>
2023-02-22 12:02:41 +0100

    WIP

--- resources/views/forms/components/belongs-to-many-input.blade.php
+++ resources/views/forms/components/belongs-to-many-input.blade.php
@@ -16,8 +16,9 @@
         search: '',
         page: 1,
         perPage: {{ $getPerPage() }},
-        items: null,
+        items: [],
         selected: [],
+        loading: true,
         init () {
             this.state ??= [] // Insure that it uses an array
 
@@ -26,6 +27,8 @@
                 this.items = [...items[0]]
                 this.selected = Alpine.raw(this.state)
                     .map((id) => this.items.find((item) => item.id === id))
+
+                this.loading = false
             })
 
             $watch('search', () => this.page = 1)
@@ -70,110 +73,107 @@
             this.updateState()
         },
     }">
-        <template x-if="items !== null">
-            <div class="flex">
-                <div class="w-1/2 h-128 bg-white border rounded-lg overflow-hidden flex flex-col" wire:ignore>
-                    <div class="border-b p-2">
-                        <input
-                            type="text"
-                            x-model="search"
-                            placeholder="Search..."
-                            class="
-                                block transition duration-75 rounded-lg shadow-sm
-                                focus:border-primary-600 focus:ring-1 focus:ring-inset focus:ring-primary-600
-                                disabled:opacity-70 border-gray-300 w-full
-                            "
-                        >
-                    </div>
-
-                    <div class="overflow-y-auto flex-auto">
-                        <template x-for="item in currentPage()" :key="item.id">
-                            <div
-                                x-html="item.html"
-                                @click="toggle(item)"
-                                class="border-b last:border-b-0 cursor-pointer"
-                            ></div>
-                        </template>
-                    </div>
-
-                    <div
-                        class="flex justify-center items-center border-t"
-                        x-show="unselected().length > perPage"
-                        x-cloak
+        <div class="flex" x-show="! loading" x-cloak>
+            <div class="w-1/2 h-128 bg-white border rounded-lg overflow-hidden flex flex-col">
+                <div class="border-b p-2">
+                    <input
+                        type="text"
+                        x-model="search"
+                        placeholder="Search..."
+                        class="
+                            block transition duration-75 rounded-lg shadow-sm
+                            focus:border-primary-600 focus:ring-1 focus:ring-inset focus:ring-primary-600
+                            disabled:opacity-70 border-gray-300 w-full
+                        "
                     >
-                        <button
-                            type="button"
-                            class="btn btn-sm btn-ghost w-1/2 flex justify-center p-4 opacity-75 hover:opacity-100 disabled:opacity-10"
-                            :disabled="page === 1"
-                            @click="page = 1"
-                        >
-                            <x-heroicon-s-chevron-double-left class="w-6 h-6" />
-                        </button>
-
-                        <button
-                            type="button"
-                            class="btn btn-sm btn-ghost w-1/2 flex justify-center p-4 opacity-75 hover:opacity-100 disabled:opacity-10"
-                            :disabled="page === 1"
-                            @click="page -= 1"
-                        >
-                            <x-heroicon-s-chevron-left class="w-6 h-6" />
-                        </button>
-
-                        <div class="text-gray-500 p-4 flex justify-center">
-                            <span x-text="page"></span>
-                            /
-                            <span x-text="maxPage()"></span>
-                        </div>
-
-                        <button
-                            type="button"
-                            class="btn btn-sm btn-ghost w-1/2 flex justify-center p-4 opacity-75 hover:opacity-100 disabled:opacity-10"
-                            :disabled="page === maxPage()"
-                            @click="page += 1"
-                        >
-                            <x-heroicon-s-chevron-right class="w-6 h-6" />
-                        </button>
-
-                        <button
-                            type="button"
-                            class="btn btn-sm btn-ghost w-1/2 flex justify-center p-4 opacity-75 hover:opacity-100 disabled:opacity-10"
-                            :disabled="page === maxPage()"
-                            @click="page = maxPage()"
-                        >
-                            <x-heroicon-s-chevron-double-right class="w-6 h-6" />
-                        </button>
-                    </div>
                 </div>
 
-                <div class="mx-4 flex items-center opacity-50">
-                    <div class="flex flex-col">
-                        <x-heroicon-o-arrow-left class="w-6 h-6" />
-                        <x-heroicon-o-arrow-right class="w-6 h-6" />
-                    </div>
-                </div>
-
-                <div
-                    class="w-1/2 h-128 bg-white border rounded-lg overflow-y-auto"
-                    wire:ignore
-                    @if ($getSortable())
-                        x-sortable="selected"
-                        x-on:end="reorder($event)"
-                    @endif
-                >
-                    <template x-for="(item, key) in selected" :key="key">
+                <div class="overflow-y-auto flex-auto">
+                    <template x-for="item in currentPage()" :key="item.id">
                         <div
-                            x-sortable-handle
-                            x-sortable-item="item.id"
                             x-html="item.html"
                             @click="toggle(item)"
                             class="border-b last:border-b-0 cursor-pointer"
                         ></div>
                     </template>
                 </div>
-            </div>
-        </template>
 
-        <template x-if="items === null">
+                <div
+                    class="flex justify-center items-center border-t"
+                    x-show="unselected().length > perPage"
+                    x-cloak
+                >
+                    <button
+                        type="button"
+                        class="btn btn-sm btn-ghost w-1/2 flex justify-center p-4 opacity-75 hover:opacity-100 disabled:opacity-10"
+                        :disabled="page === 1"
+                        @click="page = 1"
+                    >
+                        <x-heroicon-s-chevron-double-left class="w-6 h-6" />
+                    </button>
+
+                    <button
+                        type="button"
+                        class="btn btn-sm btn-ghost w-1/2 flex justify-center p-4 opacity-75 hover:opacity-100 disabled:opacity-10"
+                        :disabled="page === 1"
+                        @click="page -= 1"
+                    >
+                        <x-heroicon-s-chevron-left class="w-6 h-6" />
+                    </button>
+
+                    <div class="text-gray-500 p-4 flex justify-center">
+                        <span x-text="page"></span>
+                        /
+                        <span x-text="maxPage()"></span>
+                    </div>
+
+                    <button
+                        type="button"
+                        class="btn btn-sm btn-ghost w-1/2 flex justify-center p-4 opacity-75 hover:opacity-100 disabled:opacity-10"
+                        :disabled="page === maxPage()"
+                        @click="page += 1"
+                    >
+                        <x-heroicon-s-chevron-right class="w-6 h-6" />
+                    </button>
+
+                    <button
+                        type="button"
+                        class="btn btn-sm btn-ghost w-1/2 flex justify-center p-4 opacity-75 hover:opacity-100 disabled:opacity-10"
+                        :disabled="page === maxPage()"
+                        @click="page = maxPage()"
+                    >
+                        <x-heroicon-s-chevron-double-right class="w-6 h-6" />
+                    </button>
+                </div>
+            </div>
+
+            <div class="mx-4 flex items-center opacity-50">
+                <div class="flex flex-col">
+                    <x-heroicon-o-arrow-left class="w-6 h-6" />
+                    <x-heroicon-o-arrow-right class="w-6 h-6" />
+                </div>
+            </div>
+
+            <div
+                class="w-1/2 h-128 bg-white border rounded-lg overflow-y-auto"
+                @if ($getSortable())
+                    x-sortable="selected"
+                    x-on:end="reorder($event)"
+                @endif
+            >
+                <template x-for="item in selected" :key="item.id">
+                    <div
+                        x-sortable-handle
+                        x-sortable-item="item.id"
+                        x-html="item.html"
+                        @click="toggle(item)"
+                        class="border-b last:border-b-0 cursor-pointer"
+                    ></div>
+                </template>
+            </div>
+        </div>
+
+        <template x-if="loading">
             <div class="w-1/2 h-128 border rounded-lg overflow-hidden flex justify-center items-center">
                 <x-filament-support::loading-indicator
                     class="w-10 h-10"
