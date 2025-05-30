896a58f443a82222faadf92c6b2b1062d0ddd37c
Jyrki De Neve <jyrki.deneve@gmail.com>
2025-05-30 13:55:21 +0300

    Upgrade to Filament v4

--- tests/Feature/Forms/Components/BelongsToManyInputTest.php
+++ tests/Feature/Forms/Components/BelongsToManyInputTest.php
@@ -3,22 +3,18 @@
 use Codedor\BelongsToMany\Forms\Components\BelongsToManyInput;
 use Codedor\BelongsToMany\Tests\Fixtures\Forms\Livewire;
 use Codedor\BelongsToMany\Tests\Fixtures\Models\TestModel;
-use Filament\Forms\ComponentContainer;
 
 it('can setup the input', function () {
     $field = BelongsToManyInput::make('tags')
-        ->container(ComponentContainer::make(Livewire::make()))
         ->model(TestModel::class);
 
     expect($field)
         ->relationship->toBe('tags')
-        ->getListeners()->toHaveKey('belongs-to-many::fetchItems')
         ->getRelationship()->getTable()->toBe('test_model_test_tag');
 });
 
 it('can be sortable', function () {
     $field = BelongsToManyInput::make('tags')
-        ->container(ComponentContainer::make(Livewire::make()))
         ->model(TestModel::class)
         ->sortable();
 
